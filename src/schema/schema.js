51a4ca3c0a5577be6dda6d294c3403e90900fb5a
Roland Schläfli <rolandschlaefli@gmail.com>
2017-08-23 14:07:56 +0200

    Setup basic schema with graphql-tools

--- src/schema/schema.js
+++ src/schema/schema.js
@@ -1,27 +1,69 @@
 // @flow
 
-const graphql = require('graphql')
+const { makeExecutableSchema } = require('graphql-tools')
 
-const { GraphQLID, GraphQLObjectType, GraphQLSchema } = graphql
+const { QuestionModel, SessionModel, TagModel, UserModel } = require('../models')
 
-const mutation = require('./mutations')
-const UserModel = require('../models/User')
-const UserType = require('./types/User')
+// create graphql schema in schema language
+const typeDefs = `
+  type Question {
+    id: ID
+  }
 
-const RootQuery = new GraphQLObjectType({
-  name: 'RootQueryType',
-  fields: {
-    user: {
-      type: UserType,
-      args: { id: { type: GraphQLID } },
-      resolve(parentValue, args) {
-        return UserModel.findOne(args.id)
-      },
-    },
+  type Session {
+    id: ID
+  }
+
+  type Tag {
+    id: ID
+  }
+
+  type User {
+    id: ID
+    email: String
+    shortname: String
+
+    isActive: Boolean
+    isAAI: Boolean
+
+    questions: [Question]
+    sessions: [Session]
+    tags: [Tag]
+
+    createdAt: String
+    updatedAt: String
+  }
+
+
+  type Query {
+    allQuestions: [Question]
+    allSessions: [Session]
+    allTags: [Tag]
+    user(id: ID): User
+  }
+
+  type Mutation {
+    createUser(email: String, password: String, shortname: String): User
+  }
+
+  schema {
+    query: Query
+    mutation: Mutation
+  }
+`
+
+// define graphql resolvers for schema above
+const resolvers = {
+  Query: {
+    allQuestions: async () => QuestionModel.find({}),
+    allSessions: async () => SessionModel.find({}),
+    allTags: async () => TagModel.find({}),
+    user: async (root, { id }) => UserModel.findById(id),
   },
-})
+}
 
-module.exports = new GraphQLSchema({
-  query: RootQuery,
-  mutation,
+// use graphql-tools to generate a usable schema for export
+module.exports = makeExecutableSchema({
+  typeDefs,
+  resolvers,
 })
