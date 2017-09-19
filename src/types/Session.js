355f32c73a540cd9bbc1cbd783f35941a8c9891f
Roland Schläfli <rolandschlaefli@gmail.com>
2017-09-19 18:21:29 +0200

    Implement basic logic for session creation with automatic question instance creation

--- src/types/Session.js
+++ src/types/Session.js
@@ -1,9 +1,10 @@
 /* eslint-disable no-use-before-define */
 
 // HACK: export before require such that circular dependencies can be handled
-module.exports = () => [Session, Feedback]
+module.exports = () => [Session, Feedback, QuestionInstance]
 
 const Feedback = require('./Feedback')
+const QuestionInstance = require('./QuestionInstance')
 
 const Session = `
   enum SessionStatus {
@@ -12,14 +13,27 @@ const Session = `
     COMPLETED
   }
 
+  input Session_QuestionInput {
+    id: ID!
+  }
+
+  input QuestionBlockInput {
+    questions: [Session_QuestionInput]
+  }
+
+  input SessionInput {
+    name: String!
+    blocks: [QuestionBlockInput]!
+  }
+
   type SessionSettings {
     isConfusionBarometerActive: Boolean
     isFeedbackChannelActive: Boolean
     isFeedbackChannelPublic: Boolean
   }
 
-  input SessionInput {
-    name: String
+  type QuestionBlock {
+    questions: [QuestionInstance]
   }
 
   type Session {
@@ -29,6 +43,7 @@ const Session = `
     status: Int!
     settings: SessionSettings
 
+    blocks: [QuestionBlock]
     feedbacks: [Feedback]
 
     createdAt: String
