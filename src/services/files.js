8711ffff72004764c2dbff975cec4a3797f2d7ef
Roland Schlaefli <rolandschlaefli@gmail.com>
2018-07-21 13:35:49 +0200

    Add logic for error handling and file type constraints

--- src/services/files.js
+++ src/services/files.js
@@ -1,19 +1,40 @@
 const AWS = require('aws-sdk')
 const UUID = require('uuid/v4')
+const { UserInputError, ForbiddenError } = require('apollo-server-express')
 
-const S3 = new AWS.S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY,
-  endpoint: new AWS.Endpoint(process.env.AWS_ENDPOINT),
-  secretAccessKey: process.env.AWS_SECRET_KEY,
-})
+let S3
+if (
+  process.env.S3_ACCESS_KEY
+  && process.env.S3_ENDPOINT
+  && process.env.S3_SECRET_KEY
+) {
+  S3 = new AWS.S3({
+    accessKeyId: process.env.S3_ACCESS_KEY,
+    endpoint: new AWS.Endpoint(process.env.S3_ENDPOINT),
+    secretAccessKey: process.env.S3_SECRET_KEY,
+  })
+}
 
-const requestPresignedURL = async ({ userId, questionId }) => {
-  // TODO: compute a sensible scoped filename
+const requestPresignedURL = async ({ fileType, userId, questionId }) => {
+  // ensure that S3 is available in the environment
+  if (typeof S3 === 'undefined') {
+    throw new ForbiddenError('S3 not available.')
+  }
+
+  // compute a scoped filename
+  // images are scoped to the question as they can be reused across versions
   const fileName = `${userId}/${questionId}/${UUID()}`
 
+  // define the list of allowed file types
+  const allowedFileTypes = ['png', 'jpeg', 'gif']
+  if (!allowedFileTypes.includes(fileType)) {
+    throw new UserInputError('Unsupported file type.')
+  }
+
+  // generate a presigned url with the specified file type and generated name
   return S3.getSignedUrl('putObject', {
-    Bucket: process.env.AWS_BUCKET,
-    // ContentType: 'image/png',
+    Bucket: process.env.S3_BUCKET,
+    ContentType: `image/${fileType}`,
     Key: fileName,
   })
 }
