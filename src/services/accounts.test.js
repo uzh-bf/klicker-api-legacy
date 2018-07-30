f2912bf7d2b800c8d75f9d79b8c12805c6b6676b
Roland Schlaefli <rolandschlaefli@gmail.com>
2018-07-30 18:30:05 +0200

    Initial stubs for user data updates

---
+++ src/services/accounts.test.js
@@ -0,0 +1,129 @@
+require('dotenv').config()
+
+const { UserInputError } = require('apollo-server-express')
+const mongoose = require('mongoose')
+
+const AccountService = require('./accounts')
+const { initializeDb } = require('../lib/test/setup')
+
+mongoose.Promise = require('bluebird')
+
+describe('AccountService', () => {
+  let userId
+
+  beforeAll(async () => {
+    await initializeDb({
+      mongoose,
+      email: 'existinguser@bf.uzh.ch',
+      shortname: 'exusr',
+      withLogin: true,
+    })
+    ;({ userId } = await initializeDb({
+      mongoose,
+      email: 'testaccounts3@bf.uzh.ch',
+      shortname: 'accnts3',
+      withLogin: true,
+    }))
+  })
+
+  afterAll(async done => {
+    mongoose.disconnect(done)
+    userId = undefined
+  })
+
+  describe('checkAvailability', () => {
+    it('throws on invalid email', async () => {
+      expect(
+        AccountService.checkAvailability({
+          email: 'testblaa',
+          shortname: 'testblee',
+        })
+      ).rejects.toThrow(new UserInputError('INVALID_INPUT'))
+    })
+
+    it('throws on invalid shortname', async () => {
+      expect(
+        AccountService.checkAvailability({
+          email: 'testblaa@bf.uzh.ch',
+          shortname: 'A',
+        })
+      ).rejects.toThrow(new UserInputError('INVALID_INPUT'))
+    })
+
+    it('returns a correct response for valid email', async () => {
+      const result = await AccountService.checkAvailability({
+        email: 'testaccounts3@bf.uzh.ch',
+        shortname: undefined,
+      })
+      expect(result.email).toBeFalsy()
+      expect(result.shortname).toBeUndefined()
+    })
+
+    it('returns a correct response for valid shortname', async () => {
+      const result = await AccountService.checkAvailability({
+        email: undefined,
+        shortname: 'accnts3',
+      })
+      expect(result.email).toBeUndefined()
+      expect(result.shortname).toBeFalsy()
+    })
+
+    it('returns a correct response for valid email and shortname', async () => {
+      const result = await AccountService.checkAvailability({
+        email: 'testblaa@bf.uzh.ch',
+        shortname: 'testblee',
+      })
+      expect(result.email).toBeTruthy()
+      expect(result.shortname).toBeTruthy()
+    })
+  })
+
+  describe('updateAccountData', () => {
+    it('prevents changing the email address to an already existing one', async () => {
+      expect(
+        AccountService.updateAccountData({
+          userId,
+          email: 'existinguser@bf.uzh.ch',
+        })
+      ).rejects.toThrow(new UserInputError('EMAIL_NOT_AVAILABLE'))
+    })
+
+    it('prevents changing the shortname to an already existing one', async () => {
+      expect(
+        AccountService.updateAccountData({
+          userId,
+          shortname: 'exusr',
+        })
+      ).rejects.toThrow(new UserInputError('SHORTNAME_NOT_AVAILABLE'))
+    })
+
+    it('prevents changing the email address to an invalid value', async () => {
+      expect(
+        AccountService.updateAccountData({
+          userId,
+          email: 'invalidEmail@',
+        })
+      ).rejects.toThrow(new UserInputError('INVALID_INPUT'))
+    })
+
+    it('prevents changing the shortname to an invalid value', async () => {
+      expect(
+        AccountService.updateAccountData({
+          userId,
+          shortname: 'B',
+        })
+      ).rejects.toThrow(new UserInputError('INVALID_INPUT'))
+    })
+
+    it('allows changing user data with valid values', async () => {
+      expect(
+        AccountService.updateAccountData({
+          userId,
+          shortname: 'accnts4',
+          institution: 'testing4',
+          useCase: 'something4',
+        })
+      ).resolves.toMatchSnapshot()
+    })
+  })
+})
