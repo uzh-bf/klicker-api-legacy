7868a0f94563c30ecdddf017fff3f8d50fa07141
Roland Schlaefli <rolandschlaefli@gmail.com>
2020-05-12 17:35:35 +0200

    :card_file_box: Add String ids for participant identification

--- src/models/SessionParticipant.js
+++ src/models/SessionParticipant.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose')
 const { ObjectId } = mongoose.Schema.Types
 
 module.exports = new mongoose.Schema({
+  _id: { type: String, required: true },
+
   // the session that the participant belongs to
   session: { type: ObjectId, ref: 'Session', required: true },
 
