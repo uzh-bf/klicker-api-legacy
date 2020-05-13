089199818474c2ee5fcd59c973264f1ee53599db
Roland Schlaefli <rolandschlaefli@gmail.com>
2020-05-13 09:43:03 +0200

    Move scripts to the top-level

--- scripts/SCtoMC.js
+++ scripts/SCtoMC.js
@@ -3,7 +3,7 @@
 require('dotenv').config()
 
 const mongoose = require('mongoose')
-const { QuestionModel, UserModel } = require('../models')
+const { QuestionModel, UserModel } = require('../src/models')
 
 mongoose.Promise = Promise
 
